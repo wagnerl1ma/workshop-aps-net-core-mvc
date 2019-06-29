b53c706d7bba4d013c1b49b37a4ee0988aa25345
wagnerl1ma <wagner.araujo92@outlook.com>
2019-06-29 20:45:18 -0300

    Tratamento de exceção para delete (integridade referencial)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -36,9 +36,16 @@ namespace SalesWebMVC.Services
 
         public async Task RemoveAsync (int id)
         {
-            var obj = await _context.Vendedor.FindAsync(id);
-            _context.Vendedor.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Vendedor.FindAsync(id);
+                _context.Vendedor.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Não é possível deletar o vendedor porque ele/ela contém vendas.");
+            }
         }
 
         public async Task UpdateAsync(Vendedor obj)
